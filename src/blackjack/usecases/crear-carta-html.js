824e4b6bfb785a8a7f0f69b07fcbe1e29995840b
jorgeDevCod <jorge2698.jgpm@gmail.com>
2024-06-20 20:40:07 -0500

    update style

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -12,6 +12,7 @@ export const crearCartaHTML = ( carta ) => {
     const imgCarta = document.createElement('img');
     imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');
+    imgCarta.classList.add('img-fluid');
 
     return imgCarta;
 }
--- vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig, difineConfig } from 'vite';
-
-export default defineConfig({
-    base: '/Js-Vite-Game-BlackJack/'
-})
